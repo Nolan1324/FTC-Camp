4f3b980fdd0199ecafffcead8b8b55135c36fb13
Nolan1324 <nkuza8@gmail.com>
2017-07-12 13:40:36 -0400

    Add files via upload

---
+++ With Comments/TimeDrive.java
@@ -0,0 +1,66 @@
+//The following line is automatically created when a new program is created
+package org.firstinspires.ftc.teamcode;
+
+//Import statments automatically created when hitting
+// Alt+Enter after typing a new type of object (e.x. DcMotor, Servo)
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+//Before reading this, make sure you understand "Structure.java" and "TwoWheelDrive.java"
+
+//Register the program on the driver station with the name "TimeDrive"
+@Autonomous(name="TimeDrive")
+
+//Set the class name and make it a LinearOpMode
+public class TimeDrive extends LinearOpMode {
+    //Create DcMotor variables for the wheels
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+
+    //Define an ElapsedTime variable called "runtime" and set it to a new ElapsedTime object.
+    //To create a new object, the syntax is new ObjectName(parameters);
+    //This is must likley the only time you will see a constructor in FTC, if you keep to the basics.
+    ElapsedTime runtime = new ElapsedTime();
+
+    //A method built into LinearOpMode called "runOpMode". Unlike "loop", this will run only once.
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Init code will go before waitForStart();
+
+        //Map the wheel variables to their respective config names
+        leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        rightDrive = hardwareMap.dcMotor.get("rightDrive");
+
+        //Reverse the right drive motor
+        rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //Call a method that waits for the match to start.
+        waitForStart();
+
+        //Autonomous code:
+
+        //Start moving the wheels
+        leftDrive.setPower(0.5);
+        rightDrive.setPower(0.5);
+
+        //Reset the timer
+        runtime.reset();
+
+        //A while block. While keep runing as long the statment in ()s after "while" is true
+        //All LinearOpMode loops should contain "opModeIsActive() &&". After "&&", put your condition.
+        //In this case, the condition is if the time is less 1.0 second. If the time exceeds 1.0 second, the conditon
+        // becomes false and the program can move on.
+        while (opModeIsActive() && (runtime.seconds() < 1.0)) {
+            //Wait
+            idle();
+        }
+
+        //Stop moving the wheels
+        leftDrive.setPower(0.0);
+        rightDrive.setPower(0.0);
+    }
+}
---
+++ With Comments/TwoWheelDrive.java
@@ -0,0 +1,72 @@
+//The following line is automatically created when a new program is created
+package org.firstinspires.ftc.teamcode;
+
+//Import statments automatically created when hitting
+// Alt+Enter after typing a new type of object (e.x. DcMotor, Servo)
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+//Make sure you understand "Structure.java" before reading this.
+
+//Register the program on the driver station with the name "Basic"
+@TeleOp(name="Basic")
+
+//Set the class name and make it an OpMode
+public class TwoWheelDrive extends OpMode {
+
+    //Create DcMotor variables for the wheels
+    DcMotor leftDrive;
+    DcMotor rightDrive;
+
+    //Create a Servo variable for an arm
+    Servo arm;
+
+    //Create a decimal variable to store general power
+    //The world "final" means that the variable is a constant (i.e. it cannot be editing elsewhere in the code)
+    final double power = 0.75;
+
+    @Override
+    public void init() {
+        //Map the wheel variables to their respect config names
+        leftDrive = hardwareMap.dcMotor.get("leftDrive");
+        rightDrive = hardwareMap.dcMotor.get("rightDrive");
+
+        //Map the arm variable to its respective config name
+        arm = hardwareMap.servo.get("arm");
+
+        //Reverse the right drive motor
+        rightDrive.setDirection(DcMotorSimple.Direction.REVERSE);
+    }
+
+    @Override
+    public void loop() {
+        //Set the power of the wheels to the joy stick values multiplied by the power constant
+        leftDrive.setPower(gamepad1.left_stick_y);
+        rightDrive.setPower(gamepad1.right_stick_y);
+
+        if(gamepad1.a) {
+            //If "a" is being pressed:
+
+            //Set the arm's servo to its maximum rotation
+            arm.setPosition(1.0);
+        } else if(gamepad1.b) {
+            //If "b" is being pressed:
+
+            //Set the arm's servo to its minimum rotation
+            arm.setPosition(-1.0);
+        }
+
+        //An example of using a telemetry to display info on the
+        // driver station. This feature is meant for debugging
+        //This example displays the left joystick's x coordinate as "Left Power".
+        // This may be used, for example, testing if the controller works or
+        // seeing what values the joystick returns.
+        telemetry.addData("Left Power", gamepad1.left_stick_y);
+
+        //Update the added telemetries so they display on the driver station
+        telemetry.update();
+    }
+}
